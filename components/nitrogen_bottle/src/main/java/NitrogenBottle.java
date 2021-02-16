28bb339f9c53161a212e7675c58c9a7a5b238554
Kilian Krampf <kilian@krampf.de>
2021-02-16 18:45:27 +0100

    Implement NitrogenBottle

---
+++ components/nitrogen_bottle/src/main/java/NitrogenBottle.java
@@ -0,0 +1,65 @@
+public class NitrogenBottle {
+    // static instance
+    private static NitrogenBottle instance = new NitrogenBottle();
+    // port
+    public Port port;
+    private String manufacturer = "";
+    private String type = "team 17";
+    private String id = "2627585";
+    private int amount = 250;
+
+    // private constructor
+    private NitrogenBottle () {
+        port = new Port();
+    }
+
+    // static method getInstance
+    public static NitrogenBottle getInstance() {
+        return instance;
+    }
+
+    // inner methods
+    public String innerVersion() {
+        return "WeatherRadar // " + manufacturer + " - " + type + " - " + id;
+    }
+
+    public int innerTakeOut (int amount) {
+        if(this.amount < amount)
+            throw new RuntimeException("Insufficient Nitrogen!");
+        this.amount -= amount;
+        return this.amount;
+    }
+
+    public int innerRefill () {
+        this.amount = 250;
+        return this.amount;
+    }
+
+    public int innerRefill (int amount) {
+        this.amount = Math.min(this.amount + amount,250);
+        return this.amount;
+    }
+
+
+    // inner class port
+    public class Port implements INitrogenBottle {
+        public String version() {
+            return innerVersion();
+        }
+
+        @Override
+        public int takeOut (int amount) {
+            return innerTakeOut(amount);
+        }
+
+        @Override
+        public int refill () {
+            return innerRefill();
+        }
+
+        @Override
+        public int refill (int amount) {
+            return innerRefill(amount);
+        }
+    }
+}
