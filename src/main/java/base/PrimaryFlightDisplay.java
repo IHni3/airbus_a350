fe3de5f3afa251d8a69fba7bab159898579070fa
Kilian Krampf <kilian@krampf.de>
2021-02-16 20:28:45 +0100

    Implement PrimaryFlightDisplay values

--- src/main/java/base/PrimaryFlightDisplay.java
+++ src/main/java/base/PrimaryFlightDisplay.java
@@ -3,4 +3,27 @@ package base;
 public enum PrimaryFlightDisplay {
     instance;
     public boolean isWeatherRadarOn;
+
+    // camera
+    public boolean isBodyCameraOn;
+    public boolean isWingCameraOn;
+
+    // gps
+    public boolean isGPSOn;
+    public boolean isGPSConnected;
+
+    // nitrogen_bottle
+    public int amountOfNitrogen;
+
+    // oxygen_bottle
+    public int amountOfOxygen;
+
+    // tcas
+    public boolean isTCASOn;
+    public boolean isTCASConnected;
+    public boolean isTCASAlarm;
+    public int altitudeTCAS;
+
+    // turbulent_air_flow_alarm
+    public boolean isTurbulentAirFlowAlarm;
 }
