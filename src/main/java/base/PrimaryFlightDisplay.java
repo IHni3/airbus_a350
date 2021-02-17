81616c5324caf5ac6bda2b2076a6780d65f1dc6e
ProphetLamb <rene.carannante@outlook.de>
2021-02-17 16:15:50 +0100

    Update GUI

--- src/main/java/base/PrimaryFlightDisplay.java
+++ src/main/java/base/PrimaryFlightDisplay.java
@@ -3,4 +3,24 @@ package base;
 public enum PrimaryFlightDisplay {
     instance;
     public boolean isWeatherRadarOn;
+    public boolean isRightNavigationLightOn;
+    public boolean isTailNavigationLightOn;
+    public boolean isTaxiLightOn;
+    public boolean isNonSmokingSignOn;
+    public boolean isSeatBeltSignOn;
+    public int levelSeat;
+    public int temperatureExhaustGasTemperatureSensor;
+    public boolean isAlarmMajorExhaustGasTemperatureSensor;
+    public boolean isAlarmCriticalExhaustGasTemperatureSensor;
+    public int fuelFlow;
+    public double amountOfFuel;
+    public boolean isAlarmReserveFuelSensor;
+    public boolean isAlarmMajorFuelSensor;
+    public boolean isAlarmCriticalFuelSensor;
+    public boolean isIceDetectorProbeBodyActivated;
+    public boolean isIceDetectorProbeWingActivated;
+    public boolean isIceDetected;
+    public boolean isFireDetectedBody;
+    public boolean isFireDetectedWing;
+    public boolean isOxgenSensorAlarm;
 }
