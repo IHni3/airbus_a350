c46dcef3341f0b8a312dac60a7925ab4d6bec529
Siphalor <info@siphalor.de>
2021-02-17 11:42:05 +0100

    Small Cleanup

--- src/main/java/factory/WeatherRadarFactory.java
+++ src/main/java/factory/WeatherRadarFactory.java
@@ -1,11 +1,6 @@
 package factory;
 
 import configuration.Configuration;
-import recorder.FlightRecorder;
-
-import java.io.File;
-import java.net.URL;
-import java.net.URLClassLoader;
 
 public class WeatherRadarFactory {
 
