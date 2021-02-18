cfd8fe499e1667442a3895819cc0e50262a586b1
Siphalor <info@siphalor.de>
2021-02-18 10:40:04 +0100

    Send Airplane events for T08 and T09

--- src/main/java/base/Airplane.java
+++ src/main/java/base/Airplane.java
@@ -2,12 +2,23 @@ package base;
 
 import com.google.common.eventbus.EventBus;
 import event.Subscriber;
+import event.business_class_seat.NonSmokingSignOff;
+import event.business_class_seat.NonSmokingSignOn;
+import event.business_class_seat.SeatBeltSignOff;
+import event.business_class_seat.SeatBeltSignOn;
 import event.exhaust_gas_temperature_sensor.ExhaustGasTemperatureSensorMeasure;
+import event.fire_detector.FireDetectorBodyScan;
 import event.fuel_flow_sensor.FuelFlowSensorMeasure;
+import event.ice_detector_probe.IceDetectorProbeBodyActivate;
+import event.ice_detector_probe.IceDetectorProbeBodyDeactivate;
+import event.ice_detector_probe.IceDetectorProbeWingActivate;
+import event.ice_detector_probe.IceDetectorProbeWingDeactivate;
 import event.right_navigation_light.RightNavigationLightOff;
 import event.right_navigation_light.RightNavigationLightOn;
 import event.tail_navigation_light.TailNavigationLightOff;
 import event.tail_navigation_light.TailNavigationLightOn;
+import event.taxi_light.TaxiLightOff;
+import event.taxi_light.TaxiLightOn;
 import event.weather_radar.WeatherRadarOff;
 import event.weather_radar.WeatherRadarOn;
 import section.Body;
@@ -45,6 +56,15 @@ public class Airplane implements IAirplane {
         eventBus.post(new FuelFlowSensorMeasure());
         eventBus.post(new RightNavigationLightOn());
         eventBus.post(new TailNavigationLightOff());
+        eventBus.post(new NonSmokingSignOn());
+        eventBus.post(new SeatBeltSignOff());
+        eventBus.post(new event.crew_seat.NonSmokingSignOn());
+        eventBus.post(new event.crew_seat.SeatBeltSignOff());
+        eventBus.post(new event.economy_class_seat.NonSmokingSignOn());
+        eventBus.post(new event.economy_class_seat.SeatBeltSignOff());
+        eventBus.post(new IceDetectorProbeBodyActivate());
+        eventBus.post(new IceDetectorProbeWingActivate());
+        eventBus.post(new TaxiLightOff());
     }
 
     public void taxi() {
@@ -53,7 +73,16 @@ public class Airplane implements IAirplane {
         eventBus.post(new ExhaustGasTemperatureSensorMeasure());
         eventBus.post(new FuelFlowSensorMeasure());
         eventBus.post(new RightNavigationLightOn());
-        eventBus.post(new TailNavigationLightOn());
+        eventBus.post(new TailNavigationLightOff());
+        eventBus.post(new NonSmokingSignOn());
+        eventBus.post(new SeatBeltSignOn());
+        eventBus.post(new event.crew_seat.NonSmokingSignOn());
+        eventBus.post(new event.crew_seat.SeatBeltSignOff());
+        eventBus.post(new event.economy_class_seat.NonSmokingSignOn());
+        eventBus.post(new event.economy_class_seat.SeatBeltSignOn());
+        eventBus.post(new IceDetectorProbeBodyActivate());
+        eventBus.post(new IceDetectorProbeWingActivate());
+        eventBus.post(new TaxiLightOn());
     }
 
     public void takeoff() {
@@ -63,6 +92,15 @@ public class Airplane implements IAirplane {
         eventBus.post(new FuelFlowSensorMeasure());
         eventBus.post(new RightNavigationLightOn());
         eventBus.post(new TailNavigationLightOn());
+        eventBus.post(new NonSmokingSignOn());
+        eventBus.post(new SeatBeltSignOn());
+        eventBus.post(new event.crew_seat.NonSmokingSignOn());
+        eventBus.post(new event.crew_seat.SeatBeltSignOn());
+        eventBus.post(new event.economy_class_seat.NonSmokingSignOn());
+        eventBus.post(new event.economy_class_seat.SeatBeltSignOn());
+        eventBus.post(new IceDetectorProbeBodyActivate());
+        eventBus.post(new IceDetectorProbeWingActivate());
+        eventBus.post(new TaxiLightOff());
     }
 
     public void climbing() {
@@ -72,6 +110,15 @@ public class Airplane implements IAirplane {
         eventBus.post(new FuelFlowSensorMeasure());
         eventBus.post(new RightNavigationLightOn());
         eventBus.post(new TailNavigationLightOn());
+        eventBus.post(new NonSmokingSignOn());
+        eventBus.post(new SeatBeltSignOff());
+        eventBus.post(new event.crew_seat.NonSmokingSignOn());
+        eventBus.post(new event.crew_seat.SeatBeltSignOff());
+        eventBus.post(new event.economy_class_seat.NonSmokingSignOn());
+        eventBus.post(new event.economy_class_seat.SeatBeltSignOff());
+        eventBus.post(new IceDetectorProbeBodyActivate());
+        eventBus.post(new IceDetectorProbeWingActivate());
+        eventBus.post(new TaxiLightOff());
     }
 
     public void rightTurn() {
@@ -81,6 +128,15 @@ public class Airplane implements IAirplane {
         eventBus.post(new FuelFlowSensorMeasure());
         eventBus.post(new RightNavigationLightOn());
         eventBus.post(new TailNavigationLightOn());
+        eventBus.post(new NonSmokingSignOn());
+        eventBus.post(new SeatBeltSignOff());
+        eventBus.post(new event.crew_seat.NonSmokingSignOn());
+        eventBus.post(new event.crew_seat.SeatBeltSignOff());
+        eventBus.post(new event.economy_class_seat.NonSmokingSignOn());
+        eventBus.post(new event.economy_class_seat.SeatBeltSignOff());
+        eventBus.post(new IceDetectorProbeBodyActivate());
+        eventBus.post(new IceDetectorProbeWingActivate());
+        eventBus.post(new TaxiLightOff());
     }
 
     public void leftTurn() {
@@ -90,6 +146,15 @@ public class Airplane implements IAirplane {
         eventBus.post(new FuelFlowSensorMeasure());
         eventBus.post(new RightNavigationLightOn());
         eventBus.post(new TailNavigationLightOn());
+        eventBus.post(new NonSmokingSignOn());
+        eventBus.post(new SeatBeltSignOff());
+        eventBus.post(new event.crew_seat.NonSmokingSignOn());
+        eventBus.post(new event.crew_seat.SeatBeltSignOff());
+        eventBus.post(new event.economy_class_seat.NonSmokingSignOn());
+        eventBus.post(new event.economy_class_seat.SeatBeltSignOff());
+        eventBus.post(new IceDetectorProbeBodyActivate());
+        eventBus.post(new IceDetectorProbeWingActivate());
+        eventBus.post(new TaxiLightOff());
     }
 
     public void descent() {
@@ -99,6 +164,15 @@ public class Airplane implements IAirplane {
         eventBus.post(new FuelFlowSensorMeasure());
         eventBus.post(new RightNavigationLightOn());
         eventBus.post(new TailNavigationLightOn());
+        eventBus.post(new NonSmokingSignOn());
+        eventBus.post(new SeatBeltSignOn());
+        eventBus.post(new event.crew_seat.NonSmokingSignOn());
+        eventBus.post(new event.crew_seat.SeatBeltSignOff());
+        eventBus.post(new event.economy_class_seat.NonSmokingSignOn());
+        eventBus.post(new event.economy_class_seat.SeatBeltSignOn());
+        eventBus.post(new IceDetectorProbeBodyActivate());
+        eventBus.post(new IceDetectorProbeWingActivate());
+        eventBus.post(new TaxiLightOff());
     }
 
     public void landing() {
@@ -108,6 +182,15 @@ public class Airplane implements IAirplane {
         eventBus.post(new FuelFlowSensorMeasure());
         eventBus.post(new RightNavigationLightOn());
         eventBus.post(new TailNavigationLightOn());
+        eventBus.post(new NonSmokingSignOn());
+        eventBus.post(new SeatBeltSignOn());
+        eventBus.post(new event.crew_seat.NonSmokingSignOn());
+        eventBus.post(new event.crew_seat.SeatBeltSignOn());
+        eventBus.post(new event.economy_class_seat.NonSmokingSignOn());
+        eventBus.post(new event.economy_class_seat.SeatBeltSignOn());
+        eventBus.post(new IceDetectorProbeBodyActivate());
+        eventBus.post(new IceDetectorProbeWingActivate());
+        eventBus.post(new TaxiLightOff());
     }
 
     public void shutdown() {
@@ -117,5 +200,14 @@ public class Airplane implements IAirplane {
         eventBus.post(new FuelFlowSensorMeasure());
         eventBus.post(new RightNavigationLightOff());
         eventBus.post(new TailNavigationLightOff());
+        eventBus.post(new NonSmokingSignOff());
+        eventBus.post(new SeatBeltSignOff());
+        eventBus.post(new event.crew_seat.NonSmokingSignOff());
+        eventBus.post(new event.crew_seat.SeatBeltSignOff());
+        eventBus.post(new event.economy_class_seat.NonSmokingSignOff());
+        eventBus.post(new event.economy_class_seat.SeatBeltSignOff());
+        eventBus.post(new IceDetectorProbeBodyDeactivate());
+        eventBus.post(new IceDetectorProbeWingDeactivate());
+        eventBus.post(new TaxiLightOff());
     }
 }
