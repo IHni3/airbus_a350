8628687c770c37427fe984d05611d33ed9192c76
Siphalor <info@siphalor.de>
2021-02-18 10:19:54 +0100

    Send Airplane events for T07

--- src/main/java/base/Airplane.java
+++ src/main/java/base/Airplane.java
@@ -2,6 +2,12 @@ package base;
 
 import com.google.common.eventbus.EventBus;
 import event.Subscriber;
+import event.exhaust_gas_temperature_sensor.ExhaustGasTemperatureSensorMeasure;
+import event.fuel_flow_sensor.FuelFlowSensorMeasure;
+import event.right_navigation_light.RightNavigationLightOff;
+import event.right_navigation_light.RightNavigationLightOn;
+import event.tail_navigation_light.TailNavigationLightOff;
+import event.tail_navigation_light.TailNavigationLightOn;
 import event.weather_radar.WeatherRadarOff;
 import event.weather_radar.WeatherRadarOn;
 import section.Body;
@@ -35,45 +41,81 @@ public class Airplane implements IAirplane {
     public void startup() {
         // weather_radar
         eventBus.post(new WeatherRadarOn());
+        eventBus.post(new ExhaustGasTemperatureSensorMeasure());
+        eventBus.post(new FuelFlowSensorMeasure());
+        eventBus.post(new RightNavigationLightOn());
+        eventBus.post(new TailNavigationLightOff());
     }
 
     public void taxi() {
         // weather_radar
         eventBus.post(new WeatherRadarOn());
+        eventBus.post(new ExhaustGasTemperatureSensorMeasure());
+        eventBus.post(new FuelFlowSensorMeasure());
+        eventBus.post(new RightNavigationLightOn());
+        eventBus.post(new TailNavigationLightOn());
     }
 
     public void takeoff() {
         // weather_radar
         eventBus.post(new WeatherRadarOn());
+        eventBus.post(new ExhaustGasTemperatureSensorMeasure());
+        eventBus.post(new FuelFlowSensorMeasure());
+        eventBus.post(new RightNavigationLightOn());
+        eventBus.post(new TailNavigationLightOn());
     }
 
     public void climbing() {
         // weather_radar
         eventBus.post(new WeatherRadarOn());
+        eventBus.post(new ExhaustGasTemperatureSensorMeasure());
+        eventBus.post(new FuelFlowSensorMeasure());
+        eventBus.post(new RightNavigationLightOn());
+        eventBus.post(new TailNavigationLightOn());
     }
 
     public void rightTurn() {
         // weather_radar
         eventBus.post(new WeatherRadarOn());
+        eventBus.post(new ExhaustGasTemperatureSensorMeasure());
+        eventBus.post(new FuelFlowSensorMeasure());
+        eventBus.post(new RightNavigationLightOn());
+        eventBus.post(new TailNavigationLightOn());
     }
 
     public void leftTurn() {
         // weather_radar
         eventBus.post(new WeatherRadarOn());
+        eventBus.post(new ExhaustGasTemperatureSensorMeasure());
+        eventBus.post(new FuelFlowSensorMeasure());
+        eventBus.post(new RightNavigationLightOn());
+        eventBus.post(new TailNavigationLightOn());
     }
 
     public void descent() {
         // weather_radar
         eventBus.post(new WeatherRadarOn());
+        eventBus.post(new ExhaustGasTemperatureSensorMeasure());
+        eventBus.post(new FuelFlowSensorMeasure());
+        eventBus.post(new RightNavigationLightOn());
+        eventBus.post(new TailNavigationLightOn());
     }
 
     public void landing() {
         // weather_radar
         eventBus.post(new WeatherRadarOn());
+        eventBus.post(new ExhaustGasTemperatureSensorMeasure());
+        eventBus.post(new FuelFlowSensorMeasure());
+        eventBus.post(new RightNavigationLightOn());
+        eventBus.post(new TailNavigationLightOn());
     }
 
     public void shutdown() {
         // weather_radar
         eventBus.post(new WeatherRadarOff());
+        eventBus.post(new ExhaustGasTemperatureSensorMeasure());
+        eventBus.post(new FuelFlowSensorMeasure());
+        eventBus.post(new RightNavigationLightOff());
+        eventBus.post(new TailNavigationLightOff());
     }
 }
